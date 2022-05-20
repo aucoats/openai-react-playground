622fdb051c6ad2e432954f2f86c868caaf3b6faa
Poke Code <90790144+aucoats@users.noreply.github.com>
2022-05-20 13:19:19 -0400

    working with api

--- src/Components/Content.js
+++ src/Components/Content.js
@@ -1,21 +1,71 @@
-import React, { useState, useEffect } from "react";
+import React, { useState } from "react";
+
+// fetch("https://api.openai.com/v1/engines/text-curie-001/completions", {
+//  method: "POST",
+//  headers: {
+//    "Content-Type": "application/json",
+//    Authorization: `Bearer ${process.env.OPENAI_SECRET}`,
+//  },
+//  body: JSON.stringify(data),
+// });
+
+// const data = {
+//     prompt: "Write a poem about a dog wearing skis",
+//     temperature: 0.5,
+//     max_tokens: 64,
+//     top_p: 1.0,
+//     frequency_penalty: 0.0,
+//     presence_penalty: 0.0,
+//    };
+   
+
 
 function Content() {
 
+    // const api = 'https://api.openai.com/v1/engines/text-curie-001/completions';
     const API_KEY = process.env.REACT_APP_MY_SECRET_API_KEY;
 
-    function handleClick(e) {
+    const [paramForm, setParamForm] = useState({
+        prompt: "", 
+        temperature: .7, 
+        max_tokens: 25, 
+    })
+
+    function handlePromptBlur(e) {
+        console.log(e.target); 
+        const { name, value } = e.target; 
+        console.log(`name: ${name}, value ${value}`);
+
+        setParamForm({...paramForm, [name]: value})
+    }
+    
+    async function handleFetch(params) {
+        const data = await fetch("https://api.openai.com/v1/engines/text-curie-001/completions", {
+            method: "POST",
+            headers: {
+            "Content-Type": "application/json",
+            Authorization: `Bearer ${API_KEY}`,
+            },
+            body: JSON.stringify(params),
+            })
+
+        console.log(data);
+            
+    }
+
+    async function handleClick(e) {
         e.preventDefault();
-        const API_KEY = process.env.REACT_APP_MY_SECRET_API_KEY;
-        console.log(API_KEY);
+        const params = paramForm;
+
+        handleFetch(params);
     }
 
     return (
-        <div class="container text-center">
+        <div className="container text-center">
             <form>
-                <label class="align-self-start m-1">Enter prompt below</label><br></br>
-                <textarea id="promptArea" class="align-self-center"></textarea><br></br>
-                <button type="submit" class="btn btn-primary"
+                <label className="align-self-start m-1">Enter prompt below</label><br></br>
+                <textarea id="promptArea" name="prompt" className="align-self-center" onChange={(e) => handlePromptBlur(e)}></textarea><br></br>
+                <button type="submit" className="btn btn-primary"
                     onClick={(e) => handleClick(e)}>Submit</button>
             </form>
         </div>
